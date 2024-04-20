fe7129d10f50792b5efcce09dee7822a9934dc20
Shruthi Sundar <shruthi.sundar@january.com>
2024-04-20 16:43:43 -0700

    grr

--- test-vision/file_assistant.py
+++ test-vision/file_assistant.py
@@ -31,7 +31,7 @@ thread = client.beta.threads.create(
     messages=[
     {
       "role": "user",
-      "content": "Give me a comprehensive view of how my health has changed between these blood reports.",
+        "content": "Give me a comprehensive view of how my health has changed between these blood reports.",
     }
   ]
 )
@@ -39,62 +39,78 @@ thread = client.beta.threads.create(
 print(thread.tool_resources.file_search)
 
 
-# run = client.beta.threads.runs.create_and_poll(
-#     thread_id=thread.id, assistant_id=assistant.id
-# )
+run = client.beta.threads.runs.create_and_poll(
+    thread_id=thread.id, assistant_id=assistant.id
+)
+messages = list(client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id))
 
-# messages = list(client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id))
+while True:
+    messages = list(client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id))
+    print(messages[-1].content)
+    message_content = messages[-1].content[0].text
+    annotations = message_content.annotations
+    citations = []
+    for index, annotation in enumerate(annotations):
+        message_content.value = message_content.value.replace(annotation.text, f"[{index}]")
+        if file_citation := getattr(annotation, "file_citation", None):
+            cited_file = client.files.retrieve(file_citation.file_id)
+            citations.append(f"[{index}] {cited_file.filename}")
 
-# message_content = messages[0].content[0].text
-# annotations = message_content.annotations
-# citations = []
-# for index, annotation in enumerate(annotations):
-#     message_content.value = message_content.value.replace(annotation.text, f"[{index}]")
-#     if file_citation := getattr(annotation, "file_citation", None):
-#         cited_file = client.files.retrieve(file_citation.file_id)
-#         citations.append(f"[{index}] {cited_file.filename}")
-
-# print(message_content.value)
-# print("\n".join(citations))
+    print(message_content.value)
+    new_message = client.beta.threads.messages.create(
+        thread_id = thread.id,
+        role = "assistant",
+        content = message_content.value
+    )
+    print("put this new message in queue")
+    new_message_2 = client.beta.threads.messages.create(
+        thread_id = thread.id,
+        role = "user",
+        content = "Is my WBC high? Just say yes or no."
+    )
+    messages.append(new_message)
+    messages.append(new_message_2)
+    print("new question in queue, evaluating response")
+    #print("\n".join(citations))
 
 # Then, we use the stream SDK helper
 # with the EventHandler class to create the Run
 # and stream the response.
  
-class EventHandler(AssistantEventHandler):
-    @override
-    def on_text_created(self, text) -> None:
-        print(f"\nassistant > ", end="", flush=True)
+# class EventHandler(AssistantEventHandler):
+#     @override
+#     def on_text_created(self, text) -> None:
+#         print(f"\nassistant > ", end="", flush=True)
 
-    @override
-    def on_tool_call_created(self, tool_call):
-        print(f"\nassistant > {tool_call.type}\n", flush=True)
+#     @override
+#     def on_tool_call_created(self, tool_call):
+#         print(f"\nassistant2 > {tool_call.type}\n", flush=True)
 
-    @override
-    def on_message_done(self, message) -> None:
-        # print a citation to the file searched
-        message_content = message.content[0].text
-        annotations = message_content.annotations
-        citations = []
-        for index, annotation in enumerate(annotations):
-            message_content.value = message_content.value.replace(
-                annotation.text, f"[{index}]"
-            )
-            if file_citation := getattr(annotation, "file_citation", None):
-                cited_file = client.files.retrieve(file_citation.file_id)
-                citations.append(f"[{index}] {cited_file.filename}")
+#     @override
+#     def on_message_done(self, message) -> None:
+#         # print a citation to the file searched
+#         message_content = message.content[0].text
+#         annotations = message_content.annotations
+#         citations = []
+#         for index, annotation in enumerate(annotations):
+#             message_content.value = message_content.value.replace(
+#                 annotation.text, f"[{index}]"
+#             )
+#             if file_citation := getattr(annotation, "file_citation", None):
+#                 cited_file = client.files.retrieve(file_citation.file_id)
+#                 citations.append(f"[{index}] {cited_file.filename}")
 
-        print("on message done")
-        print(message_content.value)
-        print("\n".join(citations))
+#         print("on message done")
+#         print(message_content.value)
+#         print("\n".join(citations))
 
-print('running stream')
-with client.beta.threads.runs.stream(
-    thread_id=thread.id,
-    assistant_id=assistant.id,
-    instructions="Please address the user as Jane Doe. The user has a premium account.",
-    event_handler=EventHandler(),
-) as stream:
-    stream.until_done()
+# print('running stream')
+# with client.beta.threads.runs.stream(
+#     thread_id=thread.id,
+#     assistant_id=assistant.id,
+#     instructions="Please address the user as Jane Doe. The user has a premium account.",
+#     event_handler=EventHandler(),
+# ) as stream:
+#     stream.until_done()
 
-print("actually done")
+# print("actually done")
